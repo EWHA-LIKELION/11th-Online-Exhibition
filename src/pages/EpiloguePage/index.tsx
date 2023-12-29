0b8fda7c4a62f57e202d1239a8087e2803fa384f
서진 <dltjwls0529@gmail.com>
2023-12-30 03:16:09 +0900

    🎨 후기 페이지 헤더 적용

--- src/pages/EpiloguePage/index.tsx
+++ src/pages/EpiloguePage/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useEffect } from "react";
+import Header from "../../components/Header";
+import "./epilogue-page.scss";
 
 const EpiloguePage = () => {
-	return <div></div>;
+	useEffect(() => {
+		document
+			.querySelector("meta[name='theme-color']")
+			?.setAttribute("content", "#202020");
+	}, []);
+	return (
+		<div className="epilogue-page-wrapper">
+			<Header text="Epilogue" color="white" />
+		</div>
+	);
 };
 
 export default EpiloguePage;
